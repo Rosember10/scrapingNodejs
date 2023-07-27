d13ec8a43b72d5dfc893e55cfbb24b1f29836273
Rosember10 <rosember.01@outlook.com>
2023-07-27 13:01:12 -0400

    upload the files to gitHub

---
+++ readme.md
@@ -0,0 +1,64 @@
+# Scraping SAQ with Puppeteer
+![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=flat-square&logo=node.js&logoColor=white)
+
+Node.js script that searches the SAQ page and returns all existing bottles of wine. 
+
+
+![scraping](/project2.png)
+
+If you want to start from scratch , follow these steps : 
+
+# Installation 
+
+### Install npm 
+Run de command 
+
+> `npm init -y`
+
+### Inicialicer to create a pachage.json
+
+- create index.js file
+
+### Install modules 
+
+> npm i puppeteer
+
+> npm install node-fetch
+
+### Configure package.json
+
+- Go to package.json and add "type": "module", to be able to import modules without require
+
+# Clone 
+
+If you want to clone the project , follow these steps : 
+
+Make sure you have the latest version of node js installed (https://nodejs.org/en)
+
+### 1. Cloning the repository
+
+First, clone the repository on your local machine. Open a terminal and run the following command, replacing url-of-repository with the URL of the repository you want to clone:
+
+execute in terminal:
+
+> **git clone** project_url 
+
+### 2. Change directory to the project
+
+Once cloned, change the directory to the project by running the following command:
+
+> **cd** project_name
+
+### 3. Installing dependencies
+
+Install the project dependencies using NPM.
+
+> **npm** install
+
+### 4. Execute script
+
+Run the command 
+
+> **node** scrapingderniere.js
+
+
---
+++ scrapingderniere.js
@@ -0,0 +1,54 @@
+// https://www.youtube.com/watch?v=ssRo5nVOvrQ&t=300s 
+import puppeteer from "puppeteer";
+import fetch from "node-fetch";
+
+
+/**
+ * Extrait les informations sur les bouteilles de vin en parcourant les pages du site de la SAQ (nouveaute bouteilles )et les stocke dans un tableau.
+ * @param {number} a - La page de départ pour l'extraction des données.
+ * @param {number} b - La page finale pour l'extraction des données.
+ * @returns {Promise<void>} Une promesse qui se résout une fois que toutes les données ont été extraites.
+ */
+
+async function main(a, b) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const allProduits = [];
+    for (let i = a; i <= b; i++) {
+        let urls = 'https://www.saq.com/fr/produits?nouveaute_marketing=Nouvel+arrivage&p='+i+'&product_list_limit=96';
+        await page.goto(urls);
+        await page.waitForSelector('#maincontent > div > div.column.main > div.products.wrapper.grid.products-grid > ol > li')
+        const produits = await page.evaluate(() => {
+            const produits = document.querySelectorAll('#maincontent > div > div.column.main > div.products.wrapper.grid.products-grid > ol > li');
+            return Array.from(produits).map((produit, i) => {
+                const regexNumero = /[0-9]/;
+                const nom = produit.querySelector('.product-item-link').innerText.replace(/\d+$/, "").trim();
+                const format = produit.querySelector('strong.product.product-item-identity-format > span').innerText.split(' | ')[1].trim().split(' ')[0];
+                const prix = produit.querySelector('.price').innerText.split(' ')[0].slice(0, -2).replace(',', '.');
+                const code_saq = produit.querySelector('.saq-code > span:nth-child(2)').innerText;
+                const url_saq = produit.querySelector('.product-item-link').href;
+                const pays = produit.querySelector('strong.product.product-item-identity-format > span').innerText.split(' | ')[2].trim();
+                const type = produit.querySelector('strong.product.product-item-identity-format > span').innerText.split(' | ')[0].split(' ')[1];
+                const url_img = produit.querySelector('.product-image-photo').src;
+                const anneeVin = produit.querySelector('.product-item-link').innerText.split(' ').pop();
+                const annee = regexNumero.test(anneeVin) ? anneeVin : "";
+
+                return { nom, format, prix, annee, code_saq, url_saq, url_img, pays, type }
+
+            });
+        });
+        allProduits.push(...produits); // ajoute à chaque fois dans la variable allProduits le  produits qu'il vient de balayer
+
+    }
+    
+    await browser.close();
+    // console.log(JSON.stringify(allProduits));
+    console.log(allProduits);
+
+
+}
+
+
+main(1,2);
+
+
